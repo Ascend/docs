be1d4b9bc60a98a65ff56ca898d92750612d2bbc
Yuanhao Ji <jiyuanhao@apache.org>
2024-06-26 10:58:35 +0800

    update

--- _static/package_info.js
+++ _static/package_info.js
@@ -51,15 +51,15 @@ const package_info = {
             }
         },
         '310p': {
-            driver_version: '23.0.1',
-            firmware_version: '7.1.0.4.220',
-            firmware_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 23.0.2.1/Ascend-hdk-310p-npu-firmware_7.1.0.4.220.run',
-            kernel_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Milan-ASL/Milan-ASL V100R001C18SPC702/Ascend-cann-kernels-310p_8.0.RC2.alpha002_linux.run',
+            driver_version: '24.1.rc1',
+            firmware_version: '7.1.0.6.220',
+            firmware_url: ' https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 24.1.RC1/Ascend-hdk-310p-npu-firmware_7.1.0.6.220.run',
+            kernel_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/CANN/CANN 8.0.RC1/Ascend-cann-kernels-310p_8.0.RC1_linux.run',
             x86_64: {
-                driver_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 23.0.2.1/Ascend-hdk-310p-npu-driver_23.0.1_linux-x86-64.run',
+                driver_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 24.1.RC1/Ascend-hdk-310p-npu-driver_24.1.rc1_linux-x86-64.run',
             },
             aarch64: {
-                driver_url: 'https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 23.0.2.1/Ascend-hdk-310p-npu-driver_23.0.1_linux-aarch64.run',
+                driver_url: ' https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/Ascend HDK/Ascend HDK 24.1.RC1/Ascend-hdk-310p-npu-driver_24.1.rc1_linux-aarch64.run',
             }
         }
     },
